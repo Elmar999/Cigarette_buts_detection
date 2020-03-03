d08b15d96377094381b2f4d2aadbd69d16ee6b0d
Elmar Hajizade <44324549+Elmar999@users.noreply.github.com>
2020-03-04 01:03:37 +0400

    Add files via upload

---
+++ cigarette_buts/cigarettNet/config.py
@@ -0,0 +1,11 @@
+
+
+
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+IMAGE_DIRECTORY = '/home/elmar/Documents/cigarette_buts/out/image/buts'
+MASK_DIRECTORY = '/home/elmar/Documents/cigarette_buts/out/mask'
+BACKGROUND_DIRECTORY = '/home/elmar/Documents/cigarette_buts/out/image/bg'
+
+
