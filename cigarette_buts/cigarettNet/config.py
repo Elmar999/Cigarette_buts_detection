ee6c0969b5b88b4cccc67117b3b720d4894b6c1c
Elmar <baandicoot@gmail.com>
2020-03-04 18:20:08 +0400

    config.py has been changed

--- cigarette_buts/cigarettNet/config.py
+++ cigarette_buts/cigarettNet/config.py
@@ -1,11 +1,15 @@
+import getpass
+user = getpass.getuser()
 
 
 
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 
-IMAGE_DIRECTORY = '/home/elmar/Documents/cigarette_buts/out/image/buts'
-MASK_DIRECTORY = '/home/elmar/Documents/cigarette_buts/out/mask'
-BACKGROUND_DIRECTORY = '/home/elmar/Documents/cigarette_buts/out/image/bg'
+
+
+IMAGE_DIRECTORY = '/home/'+f'{user}'+'/arduino_proj/cigarette_buts/out/image/buts'
+MASK_DIRECTORY = '/home/'+f'{user}'+'/arduino_proj/cigarette_buts/out/mask'
+BACKGROUND_DIRECTORY = '/home/'+f'{user}'+'/arduino_proj/cigarette_buts/out/image/bg'
 
 
