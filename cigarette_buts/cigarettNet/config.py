4a5b1a27c540e38fbc5b5f0763ab4e665a2751ee
Elmar <baandicoot@gmail.com>
2020-03-04 18:21:41 +0400

    config.py has been changed

--- cigarette_buts/cigarettNet/config.py
+++ cigarette_buts/cigarettNet/config.py
@@ -8,8 +8,8 @@ IMG_HEIGHT = 256
 
 
 
-IMAGE_DIRECTORY = '/home/'+f'{user}'+'/arduino_proj/cigarette_buts/out/image/buts'
-MASK_DIRECTORY = '/home/'+f'{user}'+'/arduino_proj/cigarette_buts/out/mask'
-BACKGROUND_DIRECTORY = '/home/'+f'{user}'+'/arduino_proj/cigarette_buts/out/image/bg'
+IMAGE_DIRECTORY = '/home/'+f'{user}'+'/Cigarette_buts_detection/cigarette_buts/out/image/buts'
+MASK_DIRECTORY = '/home/'+f'{user}'+'/Cigarette_buts_detection/cigarette_buts/out/mask'
+BACKGROUND_DIRECTORY = '/home/'+f'{user}'+'/Cigarette_buts_detection/cigarette_buts/out/image/bg'
 
 
