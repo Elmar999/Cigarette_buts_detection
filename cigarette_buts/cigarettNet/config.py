445c33a053854bdcd51b3e8432f80399c941262b
Elmar <baandicoot@gmail.com>
2020-11-20 14:48:16 +0400

    refactoring

--- cigarette_buts/cigarettNet/config.py
+++ cigarette_buts/cigarettNet/config.py
@@ -3,17 +3,14 @@ import getpass
 user = getpass.getuser()
 
 
+nb_image = 3
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
-nb_image = 3
 IMG_MAX_VAL = 255
 
 
-
-
-table_name = 'paths'
 OUT_PATH = '/home/'+f'{user}'+'/Cigarette_buts_detection/cigarette_buts/out'
-DB_PATH = '/home/elmar/Cigarette_buts_detection/cigarette_buts/cigarettNet/buts.db'
+DB_PATH = '/home/'+f'{user}'+'/Cigarette_buts_detection/cigarette_buts/cigarettNet/buts.db'
 IMAGE_DIRECTORY = os.path.join(OUT_PATH, 'image/buts')
 MASK_DIRECTORY = os.path.join(OUT_PATH, 'mask')
 BACKGROUND_DIRECTORY = os.path.join(OUT_PATH, 'image/bg')
@@ -23,10 +20,9 @@ TEST_DIRECTORY = os.path.join(OUT_PATH, 'image/test')
 
 UNET_CONFIG = {
     'START_FILTER': 16,
-    'MIDDLE_FILTERS': 128,
+    'MIDDLE_FILTERS': 256,
     'DROP_RATE': 0.3,
-    'EPOCHS': 15,
+    'EPOCHS': 30,
     'BATCH_SIZE': 32,
     'NUM_THRESHOLDS': 50
 }
-
